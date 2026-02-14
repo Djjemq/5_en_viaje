a52a3e1db59f12a6de7e9dfdad830d3e79107030
Djjemq <djjemq@gmail.com>
2026-02-14 00:38:56 -0500

    album.ts

--- src/data/album.ts
+++ src/data/album.ts
@@ -5,13 +5,13 @@ export const albumData: Album = {
   artist: "Dj JEMQ",
   releaseDate: "2026",
   coverImage: "/album-cover.jpg",
-  description: "FELIZ DIA DE SAN VALENTIN MI MORZHA Y MIS HIJOS LINDOS! Este álbum habla de cómo cada uno cambia de maneras diferentes… y aun así caminamos juntos como familia en esta travesía de la vida. Este album tiene 15 canciones pero en esta primera parte les hago llegar estas 5 primeras tonadas hechas con mucho cariño, ah tambien encontraran un par de bonus tracks",
+  description: "FELIZ DIA DE SAN VALENTIN MI MORZHA Y MIS HIJOS LINDOS! Este álbum habla de cómo cada uno cambia de maneras diferentes… y aun así caminamos juntos como familia en esta travesía de la vida. Este album tiene 15 canciones pero en esta primera parte les hago llegar estas 5 primeras tonadas hechas con mucho cariño, ah tambien encontraran un par de bonus tracks.",
   songs: [
     {
       id: 3,
       title: "Au revoir",
-      duration: "0:00",
-      durationSeconds: 0,
+      duration: "4:51",
+      durationSeconds: 291,
       audioUrl: "/audio/3_Au_revoir.mp3",
       lyrics: `Solo instrumental`
     },
@@ -21,7 +21,8 @@ export const albumData: Album = {
       duration: "3:38",
       durationSeconds: 218,
       audioUrl: "/audio/8_No_es_el_lugar.mp3",
-      lyrics: `Verso 1
+      lyrics: `NO ES EL LUGAR
+      Verso 1
 Cruzamos mapas con más preguntas que certezas,
 cajas, papeles, sueños sin garantía.
 Cambió el idioma, cambió la calle,
@@ -85,26 +86,170 @@ sino para cruzarla juntos.`
       {
       id: 9,
       title: "Un pedacito de mi corazón SAMV",
-      duration: "0:00",
-      durationSeconds: 0,
+      duration: "3:47",
+      durationSeconds: 227,
       audioUrl: "/audio/8_No_es_el_lugar.mp3",
-      lyrics: `La canción estará disponible este año 2026`
+      lyrics: `UN PEDACITO DE MI CORAZÓN - Parte 1
+Verso 1 
+Nuevo pasillo, nuevo idioma,
+mismas preguntas en su mente.
+Se llena de trazos digitales que son una puerta,
+dibuja personajes mientras el día despierta.
+Sigilosa y  presente,
+calmada por fuera, fuego en la mente.
+
+Pre-coro
+Cambia el mapa, cambia el lugar,
+pero no cambia su forma de mirar.
+Si el suelo tiembla bajo sus pies,
+respira hondo… y vuelve otra vez.
+
+Coro 
+Ella no se rompe, se transforma,
+no se pierde, se reordena.
+Entre luces y cuadernos llenos,
+construye su propia escena.
+Mitad niña, mitad tormenta,
+suave voz que también enfrenta.
+
+Verso 2 
+En la net vibra en equipo,
+celebra, cae y vuelve a estar,
+y en el dojo aprende en silencio
+que la fuerza es dominar.
+Crea detalles,
+mundos pequeños que nadie invade.
+Historia viva en sus preguntas,
+¿quién mueve el mundo?, ¿quién lo apunta?
+
+Pre-coro
+Cambia el mapa, cambia el lugar,
+pero no cambia su forma de mirar.
+Si el suelo tiembla bajo sus pies,
+respira hondo… y vuelve otra vez.
+
+Coro final 
+Ella no se rompe, se transforma,
+no se pierde, se reordena.
+Entre luces y cuadernos llenos,
+construye su propia escena.
+Mitad niña, mitad tormenta,
+suave voz que también enfrenta.`
     },
     {
       id: 10,
       title: "Un pedacito de mi corazón ERMV",
       duration: "4:51",
-      durationSeconds: 0,
+      durationSeconds: 291,
       audioUrl: "/audio/10_ERMV.mp3",
-      lyrics: `La canción estará disponible este año 2026`
+      lyrics: `UN PEDACITO DE MI CORAZÓN - Parte 2
+Verso 1
+Ríe mucho antes de pensar,
+se le escapa el mundo por los ojos con solo imaginar,
+guarda tormentas de ideas en el corazón
+y las convierte en juego y fuerza con pasión.
+
+Pre-coro
+Cuando suenan cuerdas y teclas invisibles
+el día cambia de color.
+
+CORO
+Va con música en los bolsillos,
+si no entiende, lo pregunta,
+toca, prueba, rompe el ritmo,
+se equivoca y vuelve a empezar,
+busca respuestas y aprende a escuchar.
+
+Verso 2
+No necesita capa ni antifaz,
+tiene un piano atrás,
+y en cada nota musical que lo cubre
+cuando una escena de película descubre
+Pre-coro
+Cuando suenan cuerdas y teclas invisibles
+el día cambia de color.
+
+Verso 3
+Entre historietas dobladas
+aprende que ser valiente
+Cambió de calles, cambió de acento,
+no cambió su forma de mirar,
+Aprendió que el mundo es grande.
+Pre-coro
+Cuando suenan cuerdas y teclas invisibles
+el día cambia de color.`
     },
         {
       id: 11,
       title: "Un pedacito de mi corazón VZMV",
-      duration: "0:00",
+      duration: "3:39",
       durationSeconds: 0,
       audioUrl: "/audio/11_VZMV.mp3",
-      lyrics: `La canción estará disponible este año 2026`
+      lyrics: `UN PEDACITO DE MI CORAZON - Parte 3
+Verso 1
+Es la más pequeña en la fila,
+pero camina como si supiera el final.
+Mochila grande, mirada encendida,
+cada esquina es un portal.
+
+Lee una palabra y el mundo cambia,
+las letras saltan de la pared,
+la ciudad ya no es tan grande
+cuando puede entender.
+
+Pre-coro
+Tropieza, se levanta, se ríe,
+pregunta todo sin miedo a fallar,
+hay una chispa en sus pasos
+que no sabe descansar.
+
+Coro
+Ella no es pequeña por dentro,
+lleva un universo en expansión,
+gira como cinta en el viento
+con fuego en el corazón.
+
+Tan llena de vida y luz
+que todo empieza a girar,
+el mundo se abre a sus ojos
+con su diario caminar.
+
+Verso 2
+Horas y horas de disciplina,
+brilla bajo la barra de equilibrio,
+cae, respira, vuelve al ritmo,
+nunca baja el corazón.
+
+Ballet en puntas de estrella,
+jazz que la hace despegar,
+pero si hay tiempo libre corre
+sin reglas para jugar.
+
+Puente
+Tres idiomas en sus manos,
+mil historias por empezar,
+el mundo se abre a sus ojos
+cada día al caminar.
+
+No ve fronteras, ve caminos,
+no ve miedo, ve lugar.
+
+Pre-coro
+Tropieza, se levanta, se ríe,
+pregunta todo sin miedo a fallar,
+hay una chispa en sus pasos
+que no sabe descansar.
+
+Coro
+Ella no es pequeña por dentro,
+lleva un universo en expansión,
+gira como cinta en el viento
+con fuego en el corazón
+
+Tan llena de vida y luz
+que todo empieza a girar,
+el mundo se abre a sus ojos
+con su diario caminar.`
     },
                     {
       id: 16,
